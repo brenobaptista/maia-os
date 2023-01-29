fec15bd01170e79f545001c6d8c396e5539c8743
Breno Baptista <brenobaptista@protonmail.com>
2023-01-29 15:13:55 -0300

    Move file text svg into a reusable component

--- src/routes/Desktop.svelte
+++ src/routes/Desktop.svelte
@@ -1,4 +1,5 @@
 <script>
+	import FileText from './FileText.svelte';
 	import { files } from './files.js';
 	import { openedFile } from './stores.js';
 </script>
@@ -6,21 +7,7 @@
 <div class="desktop">
 	{#each files as file}
 		<button on:click={() => openedFile.set(files.indexOf(file))}>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="72"
-				height="72"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				aria-hidden="true"
-			>
-				<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-				<polyline points="14 2 14 8 20 8" />
-				<line x1="16" y1="13" x2="8" y2="13" />
-				<line x1="16" y1="17" x2="8" y2="17" />
-				<polyline points="10 9 9 9 8 9" />
-			</svg>
+			<FileText width={72} height={72} />
 			<div class="file-name">
 				{file.name}
 			</div>
