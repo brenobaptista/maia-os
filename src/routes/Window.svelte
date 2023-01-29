9e87e93f0a1419cf16b1a2a1b0c9a5899475a6c8
Breno Baptista <brenobaptista@protonmail.com>
2023-01-29 14:08:02 -0300

    Add quit button to window

--- src/routes/Window.svelte
+++ src/routes/Window.svelte
@@ -5,7 +5,10 @@
 
 {#if $openedFile !== null}
 	<div class="window">
-		<div class="title-bar">{files[$openedFile]?.name}</div>
+		<div class="title-bar">
+			<span class="title">{files[$openedFile]?.name}</span>
+			<button id="quit" class="button" on:click={() => openedFile.set(null)} />
+		</div>
 		<div class="content" />
 	</div>
 {/if}
@@ -24,10 +27,31 @@
 
 	.title-bar {
 		display: flex;
-		justify-content: center;
+		justify-content: space-between;
+		align-items: center;
 		background-color: var(--nord1);
+		border-bottom: 1px solid var(--nord16);
 		padding: 0.5rem;
 		border-radius: 0.5rem 0.5rem 0 0;
+	}
+
+	.title {
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+
+	.button {
+		border: 1px solid var(--nord16);
+		border-radius: 100%;
+		width: 0.9rem;
+		height: 0.9rem;
+	}
+
+	#quit {
+		background-color: var(--nord11);
+	}
+	#quit:hover {
+		background-color: var(--nord12);
 	}
 
 	.content {
