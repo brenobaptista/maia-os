6007fc5a40ddd891e0a862571487be3bebba249f
Breno Baptista <brenobaptista@protonmail.com>
2023-02-05 22:35:58 -0300

    Fix styles

--- src/lib/Window.svelte
+++ src/lib/Window.svelte
@@ -7,7 +7,7 @@
 <div class="window" class:window-open={$openedFile !== null}>
 	<div class="title-bar">
 		<div class="title-icon">
-			<Icon name={files[$openedFile]?.icon ?? ''} width={13} height={13} />
+			<Icon name={files[$openedFile]?.icon ?? ''} width={16} height={16} />
 			<div>{files[$openedFile]?.name ?? ''}</div>
 		</div>
 		<button id="quit" class="button" on:click={() => openedFile.set(null)} />
@@ -22,6 +22,7 @@
 		width: 320px;
 		border: 1px solid var(--nord16);
 		border-radius: 0.5rem;
+		background-color: var(--nord0);
 		box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 15px;
 		position: absolute;
 		left: 50%;
@@ -47,13 +48,14 @@
 	.title-icon {
 		display: flex;
 		gap: 0.5rem;
+		align-items: center;
 	}
 
 	.button {
 		border: 1px solid var(--nord16);
-		border-radius: 100%;
-		width: 0.9rem;
-		height: 0.9rem;
+		border-radius: 50%;
+		width: 1rem;
+		height: 1rem;
 	}
 
 	#quit {
